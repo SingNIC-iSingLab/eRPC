5a05cae83a1997654fc376e05ddbfcd8316dfbb7
Huangxy-Minel <huangxy_minel@foxmail.com>
2023-09-27 21:28:30 +0800

    sync

--- hello_world/common.h
+++ hello_world/common.h
@@ -3,8 +3,8 @@
 
 // static const std::string kServerHostname = "10.0.2.101";
 // static const std::string kClientHostname = "10.0.2.102";
-static const std::string kServerHostname = "192.168.1.211";
-static const std::string kClientHostname = "192.168.1.212";
+static const std::string kServerHostname = "192.168.0.211";
+static const std::string kClientHostname = "192.168.0.212";
 
 static constexpr uint16_t kUDPPort = 31850;
 static constexpr uint8_t kReqType = 2;

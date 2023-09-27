d9382cab8c8fe877e809cb8a03972bb796dfc66d
Huangxy-Minel <huangxy_minel@foxmail.com>
2023-09-27 18:58:37 +0800

    sync

--- hello_world/common.h
+++ hello_world/common.h
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "rpc.h"
 
-static const std::string kServerHostname = "gpu13";
-static const std::string kClientHostname = "gpu14";
+static const std::string kServerHostname = "10.0.2.101";
+static const std::string kClientHostname = "10.0.2.102";
 
 static constexpr uint16_t kUDPPort = 31850;
 static constexpr uint8_t kReqType = 2;
